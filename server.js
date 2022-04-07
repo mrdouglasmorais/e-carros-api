daeddf0a4ed4f6b513764fa7cf1c526ccc9b02a4
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-04-06 22:27:54 -0300

    update api

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
-
 const server = jsonServer.create();
-const routes = jsonServer.router('db.json');
-const middleware = jsonServer.defaults();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-server.use(middleware);
-server.use(routes);
+server.use(middlewares);
+server.use(router);
+
 server.listen(port);
